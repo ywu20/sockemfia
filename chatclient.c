1abcb892de664f64f159060778fef259779d84ce
Sarah Leong <sleong20@stuy.edu>
2022-01-11 20:53:23 -0500

    they establish connection with each other ty andy code

--- chatclient.c
+++ chatclient.c
@@ -1,1 +1,18 @@
 #include "pipe_networking.h"
+
+int main() {
+
+  int from_server;
+
+  from_server = client_handshake();
+
+  while(1){
+    char input[BUFFER_SIZE] = {0};
+    printf("Enter something...\n");
+    read(STDIN_FILENO, input, sizeof(input));
+    write(from_server, input, sizeof(input));
+    char result[BUFFER_SIZE] = {0};
+    read(from_server, result, sizeof(result));
+    printf("Result: %s\n", result);
+  }
+}
--- chatserver.c
+++ chatserver.c
@@ -1,1 +1,43 @@
 #include "pipe_networking.h"
+
+int main() {
+
+  int to_client;
+  int sd;
+
+    sd = server_setup();
+
+    printf("dng");
+
+    while (1)
+    {
+      to_client = server_connect(sd);
+
+      if (fork() == 0)
+      {
+
+      while(1){
+        char input[BUFFER_SIZE] = {0};
+        read(to_client, input, sizeof(input));
+        int i;
+        for (i = 0; i < BUFFER_SIZE && input[i]; i++)
+        {
+          input[i] += 1;
+          if (input[i] == 'a' || input[i] == 'A')
+          {
+            input[i] = '4';
+          }
+          else if (input[i] == 'e' || input[i] == 'E')
+          {
+            input[i] = '3';
+          }
+          else if (input[i] == 'I' || input[i] == 'i')
+          {
+            input[i] = '1';
+          }
+        }
+        write(to_client, input, sizeof(input));
+      }
+    }
+  }
+}
