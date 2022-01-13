a19e0f891e99474d6f97c144a5a974c85edf2bd9
Sarah Leong <sleong20@stuy.edu>
2022-01-13 00:08:13 -0500

    moved handshake back out

--- chatclient.c
+++ chatclient.c
@@ -4,18 +4,15 @@ int main() {
 
   int from_server;
 
-  while(1){
-    from_server = client_handshake();
-    printf("from_server: %d\n",from_server);
+  from_server = client_handshake();
+  printf("from_server: %d\n",from_server);
 
-    if (errno) printf("error %d: %s\n", errno, strerror(errno));
-    
-    printf("You have entered the chatroom!\n");
+  if (errno) printf("error %d: %s\n", errno, strerror(errno));
+  
+  printf("You have entered the chatroom!\n");
+  while(1){
     char input[BUFFER_SIZE] = {0};
     read(STDIN_FILENO, input, sizeof(input));
     write(from_server, input, sizeof(input));
-    // char result[BUFFER_SIZE] = {0};
-    // read(from_server, result, sizeof(result));
-    // printf("%s", result);
   }
 }
