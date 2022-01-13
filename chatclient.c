90525b0cd1a4708361d4dbf0e7975486cc7bd58b
Sarah Leong <sleong20@stuy.edu>
2022-01-13 11:38:01 -0500

    moved chat functionalities into a fxn

--- chatclient.c
+++ chatclient.c
@@ -1,4 +1,4 @@
-#include "pipe_networking.h"
+#include "chat.h"
 
 int main() {
 
@@ -10,10 +10,27 @@ int main() {
   if (errno) printf("error %d: %s\n", errno, strerror(errno));
   
   printf("You have entered the chatroom!\n");
+  chat(from_server);
+}
+
+int chat(int server) {
+  int f = fork();
+
+  if (f == 0) { // child waits for input to send
+    while (1) {
+      char input[100] = {0};
+      read(STDIN_FILENO, input, sizeof(input));
+      // fgets(input, 100, stdin);
+      write(server, input, 100);
+    }
+  }
+  // main program reads from server client msgs
   while(1){
     char input[100] = {0};
-    // read(STDIN_FILENO, input, sizeof(input));
-    fgets(input, 100, stdin);
-    write(from_server, input, 100);
+    read(server, input, sizeof(input));
+    // fgets(input, 100, stdin);
+    // write(from_server, input, 100);
+    printf("[other client]: %s\n", input);
   }
+  return 0;
 }
