7480ffffb5eb16b3b846be94c6ef717aa7ab1a2e
Sarah Leong <sleong20@stuy.edu>
2022-01-12 23:20:27 -0500

    moved code so it matches the constant reshaking of server

--- chatclient.c
+++ chatclient.c
@@ -4,18 +4,18 @@ int main() {
 
   int from_server;
 
-  from_server = client_handshake();
-  printf("from_server: %d\n",from_server);
-
-  if (errno) printf("error %d: %s\n", errno, strerror(errno));
-  
-  printf("You have entered the chatroom!\n");
   while(1){
+    from_server = client_handshake();
+    printf("from_server: %d\n",from_server);
+
+    if (errno) printf("error %d: %s\n", errno, strerror(errno));
+    
+    printf("You have entered the chatroom!\n");
     char input[BUFFER_SIZE] = {0};
     read(STDIN_FILENO, input, sizeof(input));
     write(from_server, input, sizeof(input));
-    char result[BUFFER_SIZE] = {0};
-    read(from_server, result, sizeof(result));
-    printf("%s", result);
+    // char result[BUFFER_SIZE] = {0};
+    // read(from_server, result, sizeof(result));
+    // printf("%s", result);
   }
 }
