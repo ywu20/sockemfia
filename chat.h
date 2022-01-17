c0e41164a4edeaed638fb027568f836bac6bfccf
Sarah Leong <sleong20@stuy.edu>
2022-01-16 21:25:07 -0500

    preparing to integrate with other codes

--- chat.h
+++ chat.h
@@ -4,3 +4,14 @@
 
 int chatroom(int seconds, int sd, int max_clients);
 int chat(int server);
+
+// simulating game assignment
+struct player{
+    char name [50];
+    char role[15];
+    int alive; // 1 alive, 0 dead
+    int socket;
+    int votes;
+};
+
+struct player * player_setup(char name[50], int socket);
