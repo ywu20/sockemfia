84cc76bf103ed06722f4898a2bd9a0f886c17045
Sarah Leong <sleong20@stuy.edu>
2022-01-16 19:58:18 -0500

    made clients_fds to store fd_set for write and read to copy; makes code less clunky

--- chatserver.c
+++ chatserver.c
@@ -11,7 +11,7 @@ int main() {
 }
 
 int chatroom(int seconds, int sd, int max_clients) { // seconds will but rn doesn't limit chat time
-    fd_set read_fds, write_fds;
+    fd_set read_fds, write_fds, clients_fds;
     int max_fd = sd;
     int r;
 
@@ -26,6 +26,14 @@ int chatroom(int seconds, int sd, int max_clients) { // seconds will but rn does
             max_fd = clients[i];
             printf("clients[i] %d joined\n", clients[i]);
         }
+
+        if (FD_ISSET(clients[i], &clients_fds)) { // if already in read set
+            printf("client %d was read set\n", clients[i]);
+        } else { // if not in set
+            FD_SET(clients[i], &clients_fds); // add to read set
+            printf("added fd %d to read set\n", clients[i]);
+        }
+
         i++;
     }
     printf("new max_fd: %d\n", max_fd);
@@ -35,29 +43,33 @@ int chatroom(int seconds, int sd, int max_clients) { // seconds will but rn does
         char input[100];
         FD_ZERO(&read_fds); // clears set
         FD_ZERO(&write_fds);
-        FD_SET(sd, &read_fds);  // adds server socket to set
-        FD_SET(sd, &write_fds);
 
-        // add fds to set
-        for (i = 0; i < max_clients; i++) { // adds all the fds to check up on
-            if (FD_ISSET(clients[i], &read_fds)) { // if already in read set
-                printf("client %d was read set\n", clients[i]);
-            } else { // if not in set
-                FD_SET(clients[i], &read_fds); // add to read set
-                printf("added fd %d to read set\n", clients[i]);
-            }
+        read_fds = clients_fds;
+        write_fds = clients_fds;
 
-            if (FD_ISSET(clients[i], &write_fds)) { // if already in write set
-                printf("client %d was write set\n", clients[i]);
-            } else { // if not in set
-                FD_SET(clients[i], &write_fds); // add to write set
-                printf("added fd %d to write set\n", clients[i]);
-            }
+        // FD_SET(sd, &read_fds);  // adds server socket to set
+        // FD_SET(sd, &write_fds);
 
-            if (max_fd < clients[i]) {
-                max_fd = clients[i];
-            }
-        }
+        // // add fds to set
+        // for (i = 0; i < max_clients; i++) { // adds all the fds to check up on
+        //     if (FD_ISSET(clients[i], &read_fds)) { // if already in read set
+        //         printf("client %d was read set\n", clients[i]);
+        //     } else { // if not in set
+        //         FD_SET(clients[i], &read_fds); // add to read set
+        //         printf("added fd %d to read set\n", clients[i]);
+        //     }
+
+        //     if (FD_ISSET(clients[i], &write_fds)) { // if already in write set
+        //         printf("client %d was write set\n", clients[i]);
+        //     } else { // if not in set
+        //         FD_SET(clients[i], &write_fds); // add to write set
+        //         printf("added fd %d to write set\n", clients[i]);
+        //     }
+
+        //     if (max_fd < clients[i]) {
+        //         max_fd = clients[i];
+        //     }
+        // }
 
         int sel = select(max_fd+1, &read_fds, NULL, NULL, NULL);
         printf("sel: %d\n", sel);
@@ -77,7 +89,7 @@ int chatroom(int seconds, int sd, int max_clients) { // seconds will but rn does
                     FD_CLR(clients[i], &write_fds);
                 }
             }
-            
+
              // if there is stuff left in write set
             for (int i = 0; i < max_clients && r; i++) { // loops to find the active client
                 if (FD_ISSET(clients[i], &write_fds)) { // if the client is in remaining one
