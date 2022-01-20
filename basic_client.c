13d7c42f448fb98d8e9e41bee7ef847910791b06
Sarah Leong <sleong20@stuy.edu>
2022-01-19 20:32:39 -0500

    moving chatroom into the server and client programs

--- basic_client.c
+++ basic_client.c
@@ -63,3 +63,23 @@ int main(int argc, char *argv[]) {
     free(parsedIn);
   }
 }
+
+int chat(int server) {
+  printf("You have entered the chatroom!\n");
+  char input[100];
+  int f = fork();
+
+  if (f == 0) { // child waits for input to send
+    while (read(STDIN_FILENO, input, sizeof(input))) {
+      write(server, input, 100);
+    }
+  }
+
+  // main program reads from server client msgs
+  while(read(server, input, sizeof(input))){
+    printf("%s", input);
+  }
+  kill(f, SIGKILL); // removes child process
+  printf("chatroom over\n");
+  return 0;
+}
--- basic_server.c
+++ basic_server.c
@@ -412,6 +412,115 @@ void gameCycle(int playerCount){
   }
 }
 
+int chatroom(int seconds, int sd, int max_clients, struct player * players[20]) { // seconds will but rn doesn't limit chat time
+    fd_set read_fds, write_fds, clients_fds;
+    int max_fd = sd;
+    int r;
+
+    struct timeval t = {seconds, 0};
+
+    // gather the clients
+    int clients[max_clients];
+    printf("waiting for people to connect\n"); 
+    int i = 0;
+
+    while (i<max_clients) {
+        clients[i] = players[i] -> socket;
+        if (clients[i] > max_fd) {
+            max_fd = clients[i];
+            printf("clients[%d] joined\n", clients[i]);
+        }
+
+        FD_SET(sd, &clients_fds); // add server socket to set
+        if (FD_ISSET(clients[i], &clients_fds)) { // if already in client set
+            printf("client %d was read set\n", clients[i]);
+        } else { // if not in set
+            FD_SET(clients[i], &clients_fds); // add to client set
+            printf("added fd %d to read set\n", clients[i]);
+        }
+        i++;
+    }
+    printf("new max_fd: %d\n", max_fd);
+
+    // preparing the timings
+    time_t startTime = time(NULL);
+
+    // start the chatroom
+    while (time(NULL)-startTime < seconds) {
+
+        // 10 second warning will be moved to a fork
+        if (time(NULL)-startTime <= 10.5 && time(NULL)-startTime >= 9.5) {
+            write_fds = clients_fds;
+            for (int i = 0; i < max_clients && r; i++) { // loops to find the active client
+                if (FD_ISSET(clients[i], &write_fds)) { // sends 10 second warning
+                    printf("going to write to %d: [server] 10 SECONDS LEFT TO CHAT!\n", clients[i]);
+                    write(clients[i], "[server] 10 SECONDS LEFT TO CHAT!\n", 34);
+                }
+            }
+        }
+
+        char input[100] = "";
+        char chatter[50] = "";
+        char final_message[150] = "";
+        FD_ZERO(&read_fds); // clears set
+        FD_ZERO(&write_fds);
+
+        // copy client set
+        read_fds = clients_fds;
+        write_fds = clients_fds;
+
+        int sel = select(max_fd+1, &read_fds, NULL, NULL, &t);
+        printf("sel: %d\n", sel);
+
+        if (sel) { // if there is stuff left in read set
+            for (int i = 0; i < max_clients; i++) { // loops to find the active client
+                if (FD_ISSET(clients[i], &read_fds)) { // if the client is in remaining one
+                    printf("going to read from %d\n", clients[i]);
+                    r = read(clients[i], input, 100);
+                    strncpy(chatter, players[i]->name,50);
+                    printf("got data: %s\n",input);
+                    printf("chatter: %s\n", chatter);
+                    FD_CLR(clients[i], &write_fds);
+                }
+            }
+
+            // preparing the final message
+            int len = 0;
+            for (int i = 0; i < 50; i++) {
+                if (chatter[i]!='\0') {
+                    final_message[i] = chatter[i];
+                    printf("copying %c into final msg\n", chatter[i]);
+                } else {
+                    len = i+1;
+                    final_message[i] = ':';
+                    final_message[i+1] = ' ';
+                    i = 50;
+                }
+            }
+            for (int i = 0; i < 100;i++) {
+                if (input[i]!='\n') {
+                    final_message[i+len] = input[i];
+                    printf("copying %c into final msg\n", final_message[i+len]);
+                } else {
+                    final_message[i+len] = '\n';
+                    i = 100;
+                }
+            }
+
+             // if there is stuff left in write set
+            for (int i = 0; i < max_clients && r; i++) { // loops to find the active client
+                if (FD_ISSET(clients[i], &write_fds)) { // if the client is in remaining one
+                    printf("going to write to %d: %s\n", clients[i], final_message);
+                    write(clients[i], final_message, 150);
+                }
+            }
+        }
+        printf("loop complete\n\n");
+    }
+
+    return 0;
+}
+
 int main() {
 
   int to_client;
