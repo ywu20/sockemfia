395371e176f9671893c6af87805eaab6ba1be0b9
Sarah Leong <sleong20@stuy.edu>
2022-01-19 21:42:32 -0500

    learning where to call chat, also chat was moved ehre

--- basic_client.c
+++ basic_client.c
@@ -59,6 +59,7 @@ int main(int argc, char *argv[]) {
       }
       read(STDIN_FILENO, in, sizeof(in));
       write(from_server, in, sizeof(in));
+      // chat(ipAddress);
     }
     free(parsedIn);
   }
