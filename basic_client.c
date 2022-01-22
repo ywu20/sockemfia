56455562fca2ee2225fd637c559f102c41229d37
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-01-22 18:56:14 -0500

    another lame change to be fixed

--- basic_client.c
+++ basic_client.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]) {
     else if(strcmp(parsedIn[0], NOTIFY_PLAYER) == 0){
       printf("%s\n", parsedIn[1]);
     }
-    else if(strncmp(parsedIn[0], "CHAT",5) == 0) {
+    else if(strncmp(parsedIn[0], "CHAT",4) == 0) {
       chat(from_server, parsedIn[0][4]);
     }
     else
