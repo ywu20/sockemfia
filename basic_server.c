e73338703238ec8031454418f90bd80393682bad
Sarah Leong <sleong20@stuy.edu>
2022-01-22 22:10:25 -0500

    moving client array assignment

--- basic_server.c
+++ basic_server.c
@@ -394,6 +394,7 @@ int chatroom(int seconds, int max_clients, struct player * players[20], int mafi
               write(players[i]->socket, "CHAT1", 5); // living people
               printf("told player %s to connect\n", players[i]->name);
           }
+          clients[i] = players[i]->socket;
         }
       }
       else {
@@ -404,25 +405,31 @@ int chatroom(int seconds, int max_clients, struct player * players[20], int mafi
             write(players[i]->socket, "CHAT1", 5); // living people
             printf("told player %s to connect\n", players[i]->name);
         }
+        clients[i] = players[i]->socket;
       }
+      if (clients[i] > max_fd) {
+          max_fd = clients[i];
+          printf("clients[%d] joined\n", clients[i]);
+      }
+      
     }
 
-    i = 0;
-    while (i<max_clients) {
-        clients[i] = (players[i] -> socket);
-        if (clients[i] > max_fd) {
-            max_fd = clients[i];
-            printf("clients[%d] joined\n", clients[i]);
-        }
-        // FD_SET(sd, &clients_fds); // add server socket to set
-        if (FD_ISSET(clients[i], &clients_fds)) { // if already in client set
-            printf("client %d was read set\n", clients[i]);
-        } else { // if not in set
-            FD_SET(clients[i], &clients_fds); // add to client set
-            printf("added fd %d to read set\n", clients[i]);
-        }
-        i++;
-    }
+    // i = 0;
+    // while (i<max_clients) {
+    //     clients[i] = (players[i] -> socket);
+    //     if (clients[i] > max_fd) {
+    //         max_fd = clients[i];
+    //         printf("clients[%d] joined\n", clients[i]);
+    //     }
+    //     // FD_SET(sd, &clients_fds); // add server socket to set
+    //     if (FD_ISSET(clients[i], &clients_fds)) { // if already in client set
+    //         printf("client %d was read set\n", clients[i]);
+    //     } else { // if not in set
+    //         FD_SET(clients[i], &clients_fds); // add to client set
+    //         printf("added fd %d to read set\n", clients[i]);
+    //     }
+    //     i++;
+    // }
     printf("new max_fd: %d\n", max_fd);
 
     // preparing the timings
