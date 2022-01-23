4e8f22b15734975f10e70318158c69ae0320b845
Sarah Leong <sleong20@stuy.edu>
2022-01-22 22:53:59 -0500

    help me

--- basic_server.c
+++ basic_server.c
@@ -450,20 +450,22 @@ int chatroom(int seconds, int max_clients, struct player * players[20], int mafi
         printf("sel: %d\n", sel);
         // printf("final_msg check (null): %s\n", final_message);
         int here = 0;
+        printf("here: %d\n", here);
 
         if (sel) { // if there is stuff left in read set
             for (int i = 0; i < max_clients; i++) { // loops to find the active client
-                printf("clients[%d]: %d\n", i, clients[i]);
-                if (FD_ISSET(clients[i], &read_fds)) { // if the client is in remaining one
-                    printf("going to read from %d\n", clients[i]);
-                    r = read(clients[i], input, 100);
-                    // strcpy(final_message, players[i]->name);
-                    printf("got data: %s\n",input);
-                    // printf("chatter: %s\n", chatter);
-                    // printf("final msg so far: %s\n", final_message);
-                    FD_CLR(clients[i], &write_fds);
-                    here = i;
-                }
+              printf("hello! i for looping\n")
+              printf("clients[%d]: %d\n", i, clients[i]);
+              if (FD_ISSET(clients[i], &read_fds)) { // if the client is in remaining one
+                  printf("going to read from %d\n", clients[i]);
+                  r = read(clients[i], input, 100);
+                  // strcpy(final_message, players[i]->name);
+                  printf("got data: %s\n",input);
+                  // printf("chatter: %s\n", chatter);
+                  // printf("final msg so far: %s\n", final_message);
+                  FD_CLR(clients[i], &write_fds);
+                  here = i;
+              }
             }
             
             // prepare the final message
