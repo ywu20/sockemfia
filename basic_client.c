2c8149f5751cff1842b2b5688bdb86a719fd3c9d
Sarah Leong <sleong20@stuy.edu>
2022-01-22 23:41:50 -0500

    does this empy the string im desperate

--- basic_client.c
+++ basic_client.c
@@ -89,6 +89,7 @@ int chat(int server, char living) {
     if (f == 0) { // child waits for input to send
       while (read(STDIN_FILENO, input, sizeof(input))) {
         write(server, input, 100);
+        strncpy(input, "", 100);
       }
     }
   } else {
@@ -99,6 +100,7 @@ int chat(int server, char living) {
   while(read(server, output, sizeof(input)) && strcmp(output, "STOPTALKING")){
     // input[152] = '\n';
     printf("%s", output);
+    strncpy(output, "", 152);
   }
   if (f) kill(f, SIGKILL); // removes child process
   printf("\nchatroom over\n\n");
