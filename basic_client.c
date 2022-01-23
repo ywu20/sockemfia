39e0950b7c96bb6452b0bffa090d874edd707619
Sarah Leong <sleong20@stuy.edu>
2022-01-22 20:32:35 -0500

    is this the issue?

--- basic_client.c
+++ basic_client.c
@@ -88,6 +88,9 @@ int chat(int server, char living) {
 
     if (f == 0) { // child waits for input to send
       while (read(STDIN_FILENO, input, sizeof(input))) {
+        for (int i = 0; i < sizeof(input); i++) {
+          if (input[i]=='\n') input[i] = '\0';
+        }
         write(server, input, 100);
       }
     }
